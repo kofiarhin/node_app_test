aed3f237f15936abffb825b0f46efc0b2aef5387
unknown <mysocialmedia69@yahoo.com>
2020-12-10 10:09:07 +0000

    update

--- server/app.js
+++ server/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const publicPath = path.join(__dirname, "../public");
+const mongoose = require("mongoose");
+
+// connect to database
+mongoose
+  .connect(process.env.mongo_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to database"))
+  .catch((e) => {
+    console.lg("error connecting to database!");
+  });
 
 // setup middleware
 app.use(express.static(publicPath));
